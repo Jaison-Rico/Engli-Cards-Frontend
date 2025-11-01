d6c762a1f49e4fce515230b0b0d9a3ccef823116
owenunda <oweenunda.dev@gmail.com>
2025-11-01 15:00:11 -0500

    add: api

--- src/config/api.js
+++ src/config/api.js
@@ -1,11 +1,23 @@
 const dev = {
-  BASE_URL: "https://b0d16ca9e41d.ngrok-free.app"
+  BASE_URL: "https://engli-cards-backend-production.up.railway.app"
 };
 
 const prod = {
-  BASE_URL: "https://miapi-production.render.com"
+  BASE_URL: "https://engli-cards-backend-production.up.railway.app"
 };
 
 export const config = __DEV__ ? dev : prod;
 
 // __DEV__ es una variable global de React Native que es true en modo desarrollo
+
+
+/*
+Ejemplo de usuario para pruebas:
+{
+  "name": "test",
+  "email": "test@example.com",
+  "password": "2210"
+}
+
+
+*/
