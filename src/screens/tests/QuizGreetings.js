5d1459e436c737fad5d613f25292d97d4b574908
Jaison-Rico <jaih90@aol.com>
2025-11-08 22:43:42 -0500

    Add: primera vista de la screen quiz-saludos

--- src/screens/tests/QuizGreetings.js
+++ src/screens/tests/QuizGreetings.js
@@ -1,27 +1,40 @@
 import { View, Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from '../../styles/stylesTestQuiz/StylesQGreetings';
-import * as React from 'react';
-import { ProgressBar, MD3Colors } from 'react-native-paper';
+import { ProgressBar } from 'react-native-paper';
 import { MoveLeft } from 'lucide-react-native';
 
 export default function QuizGreetings() {
   return (
     <SafeAreaView>
       <View style={styles.containerMCTop}>
-        <TouchableOpacity>
-          <Text style={styles.textButtonBackQG}> 
-            <MoveLeft size={25} color="#6200ee"  />
-            Saludos
-          </Text>
+        <TouchableOpacity style={styles.backButton}>
+          <MoveLeft size={25} color="#6200ee"  />
+          <Text style={styles.textButtonBackQG}> Saludos</Text>
         </TouchableOpacity>
           <Text style={styles.textsQG}>Pregunta 1 de 10!</Text>
-          <ProgressBar style={{ marginTop: 40 }} progress={0} color={"#6200ee"} />
+          <ProgressBar style={{ marginTop: 20 }} progress={0.5} color={"#6200ee"} />
         
       </View>
 
+      <View style={styles.containerTitleQG}>
+        <Text style={styles.textTitleQG}>Hola</Text>
+        <Text style={styles.textsQG2}>Selecciona la traducción correcta</Text>
+      </View>
+
       <View>
-        <Text style={styles.textButtonQG}>Hola</Text>
+        <TouchableOpacity style={styles.containerOptionsQG}>
+          <Text style={styles.textsQG}>Hello</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.containerOptionsQG}>
+          <Text style={styles.textsQG}>Bye</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.containerOptionsQG}>
+          <Text style={styles.textsQG}>Good Morning</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.containerOptionsQG}>
+          <Text style={styles.textsQG}>See you soon</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
--- src/styles/stylesTestQuiz/StylesQGreetings.js
+++ src/styles/stylesTestQuiz/StylesQGreetings.js
@@ -5,10 +5,16 @@ const stylesQG = StyleSheet.create({
     containerMCTop: {
         paddingLeft: 20,
         paddingRight: 20,
-        backgroundColor: theme.colors.background,
+        backgroundColor: '#FFFFFF',
         height: 100,
         borderBottomLeftRadius: tokens.radius.lg,
         borderBottomRightRadius: tokens.radius.lg,
+    },
+    backButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+        marginTop: 5,
     },
     textsQG:{
         fontSize:15,
@@ -18,14 +24,20 @@ const stylesQG = StyleSheet.create({
         marginBottom:0,
         color: theme.colors.foreground,
     },
-    textButtonQG:{
+    textsQG2:{
+        fontSize:15,
+        fontWeight:'bold',
+        textAlign:'center',
+        marginTop:15,
+        marginBottom:0,
+        color: theme.colors.foreground,
+    },
+    textTitleQG:{
         fontSize:35,
         fontWeight:'bold',
         textAlign:'center',
-        marginBottom:0,
-        marginTop: 100,
         color: '#000000',
-        marginBottom: 15,
+        marginBottom: 0,
     },
     textButtonBackQG:{
         fontSize:20,
@@ -34,7 +46,30 @@ const stylesQG = StyleSheet.create({
         marginBottom:0,
         marginTop: 0,
         color: '#000000',
-    }
+    },
+    containerTitleQG:{
+        width: "80%",
+        height: 200,
+        borderWidth: 1,
+        borderRadius: 20,
+        borderColor: 'black',
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center', // Centra verticalmente el contenido
+        alignItems: 'center', // Centra horizontalmente el contenido
+        marginTop: 50, // Espacio entre los contenedores
+        alignSelf: 'center', // Centra el contenedor en la pantalla
+        marginBottom: 60,
+    },
+    containerOptionsQG:{
+        width: "80%",
+        height: 60,
+        borderWidth: 1,
+        borderRadius: 20,
+        borderColor: 'black',
+        backgroundColor: '#FFFFFF', 
+        alignSelf: 'center',
+        marginBottom: 25,
+    },
 });
 export default stylesQG;
 
